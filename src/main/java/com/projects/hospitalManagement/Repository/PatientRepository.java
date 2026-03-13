0aa00b0294bbf77dd200cafd0292380833b54afb
ayush1125cherry <ayushrawat3243@gmail.com>
2026-03-13 16:21:55 +0530

    Initial commit - Spring Boot Hospital Management API

---
+++ src/main/java/com/projects/hospitalManagement/Repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package com.projects.hospitalManagement.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository {
+}
---
+++ src/main/java/com/projects/hospitalManagement/entity/Patient.java
@@ -0,0 +1,25 @@
+package com.projects.hospitalManagement.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate Dob;
+
+    private String email;
+
+    private String gender;
+}
